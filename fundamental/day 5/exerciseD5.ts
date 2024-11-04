b1f618f5623c46223d9a1deffb9fe863b577020b
qpratama <vitalisdeus@gmail.com>
2024-11-04 22:39:08 +0700

    feat: exercise day 5

---
+++ fundamental/day 5/exerciseD5.ts
@@ -0,0 +1,10 @@
+
+/* Day 5 - Exercise 1.1*/
+
+let arr1 = [12, 5, 23, 18, 4, 45, 32]
+
+let minArr = Math.min(...arr1)
+let maxArr = Math.max(...arr1)
+
+console.log(`lowest: ${minArr}`);
+console.log(`highest: ${maxArr}`);
